39388fff1ebeeed3a9cec6d8fc80d9e4706575a6
leonardovalde <leonardovalde16@gmail.com>
2024-03-05 11:48:33 -0500

    add counter url

--- 02-counter/webpack.config.js
+++ 02-counter/webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack')
 const deps = require('./package.json').dependencies
 module.exports = (_, argv) => ({
   output: {
-    publicPath: 'http://localhost:51166/',
+    publicPath: 'https://mf-ft4o.onrender.com/',
   },
 
   resolve: {
