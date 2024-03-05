84c903127cbacfaaf390161fd4b333d07ccdf581
leonardovalde <leonardovalde16@gmail.com>
2024-03-05 11:09:16 -0500

    add dist

--- 02-counter/src/components/CounterComponent.tsx
+++ 02-counter/src/components/CounterComponent.tsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react'
 import '../index.scss'
 
 function CounterComponent() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
   return (
-    <div>
-        Count is:        
-        <p>{count}</p>
-        <button className='w-7 h-7 bg-green-500 rounded-r-xl' onClick={() => setCount(count + 1)}>+</button>
-        <button className='w-7 h-7 bg-red-400 rounded-md' onClick={() => setCount(count - 1)}>-</button>
+    <div className="counterContainer">
+      Count is:
+      <p>{count}</p>
+      <section className="buttonsContainer">
+        <button
+          className="addBtn w-7 h-7 bg-green-500 rounded-r-xl"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button className="subBtn" onClick={() => setCount(count - 1)}>
+          -
+        </button>
+      </section>
     </div>
   )
 }
